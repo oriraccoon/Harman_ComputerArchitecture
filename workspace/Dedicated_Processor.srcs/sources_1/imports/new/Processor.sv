2423320276a7ddefc88702220b2a3bf0ec791937
oriraccoon <djdjskskalal@gmail.com>
2025-04-07 00:00:16 +0900

    dedicated_processor test sv

---
+++ workspace/Dedicated_Processor.srcs/sources_1/imports/new/Processor.sv
@@ -0,0 +1,34 @@
+module Dedicate_Processor(
+    input logic clk,
+    input logic rst,
+    input logic [3:0] repeat_num,
+    input logic [2:0] start_num,
+    output logic [7:0] outvalue
+    );
+
+    logic altb, aen, asel, outbuf;
+
+    Control_Unit U_Control_Unit(
+        .clk(clk),
+        .rst(rst),
+        .altb(altb),
+
+        .asel(asel),
+        .aen(aen),
+        .outbuf(outbuf)
+    );
+
+    Data_Path U_Data_Path(
+        .clk(clk),
+        .rst(rst),
+        .asel(asel),
+        .aen(aen),
+        .outbuf(outbuf),
+        .repeat_num2(repeat_num),
+        .start_num(start_num),
+
+        .altb(altb),
+        .outport(outvalue)
+    );
+
+endmodule
