845d9efcf5672bd2c478c879e51741a1960bc4aa
kccistc <djdjskskalal@gmail.com>
2025-04-18 16:55:18 +0900

    Auto commit for modified projects on 2025-04-18 16:55

--- workspace/RISCV_32bit_CPU_MultiCycle/RISCV_32bit_CPU_MultiCycle.srcs/sources_1/new/APB_Master.sv
+++ workspace/RISCV_32bit_CPU_MultiCycle/RISCV_32bit_CPU_MultiCycle.srcs/sources_1/new/APB_Master.sv
@@ -1,12 +1,14 @@
+`timescale 1ns / 1ps
+
 module APB_Master (
     // global signal
     input  logic        PCLK,
-    input  logic        PRST,
+    input  logic        PRESET,
     // APB Interface Signals
     output logic [31:0] PADDR,
-    output logic        PENABLE,
-    output logic        PWRITE,
     output logic [31:0] PWDATA,
+    output logic        PWRITE,
+    output logic        PENABLE,
     output logic        PSEL0,
     output logic        PSEL1,
     output logic        PSEL2,
@@ -21,12 +23,26 @@ module APB_Master (
     input  logic        PREADY3,
     // Internal Interface Signals
     input  logic        transfer,  // trigger signal
+    output logic        ready,
     input  logic [31:0] addr,
     input  logic [31:0] wdata,
-    input  logic        we,        // 1:write, 0:read
     output logic [31:0] rdata,
-    output logic        ready
+    input  logic        write      // 1:write, 0:read
 );
+    logic [31:0] temp_addr_next, temp_addr_reg;
+    logic [31:0] temp_wdata_next, temp_wdata_reg;
+    logic temp_write_next, temp_write_reg;
+    logic decoder_en;
+    logic [3:0] pselx;
+
+    assign PSEL0  = pselx[0];
+    assign PSEL1  = pselx[1];
+    assign PSEL2  = pselx[2];
+    assign PSEL3  = pselx[3];
+    assign PADDR  = temp_addr_reg;
+    assign PWDATA = temp_wdata_reg;
+
+
 
     typedef enum bit [1:0] {
         IDLE,
@@ -36,46 +52,17 @@ module APB_Master (
 
     apb_state_e state, state_next;
 
-    logic [31:0] temp_addr_reg, temp_addr_next;
-    logic [31:0] temp_wdata_reg, temp_wdata_next;
-    logic temp_we_reg, temp_we_next;
-    logic       decoer_en;
-    logic [3:0] pselx;
-
-    assign {PSEL0, PSEL1, PSEL2, PSEL3} = pselx;
-
-    APB_Decoder U_APB_Decoder (
-        .en (decoer_en),
-        .sel(temp_addr_reg),
-        .y  (pselx)
-    );
-
-    APB_Mux U_APB_Mux (
-        .sel(),
-        .d0(),
-        .d1(),
-        .d2(),
-        .d3(),
-        .r0(),
-        .r1(),
-        .r2(),
-        .r3(),
-        .rdata(),
-        .ready()
-    );
-
-
-    always_ff @(posedge PCLK or posedge PRST) begin
-        if (PRST) begin
-            state <= IDLE;
-            temp_addr_reg <= 0;
+    always_ff @(posedge PCLK, posedge PRESET) begin
+        if (PRESET) begin
+            state          <= IDLE;
+            temp_addr_reg  <= 0;
             temp_wdata_reg <= 0;
-            temp_we_reg <= 0;
+            temp_write_reg <= 0;
         end else begin
             state          <= state_next;
             temp_addr_reg  <= temp_addr_next;
             temp_wdata_reg <= temp_wdata_next;
-            temp_we_reg    <= temp_we_next;
+            temp_write_reg <= temp_write_next;
         end
     end
 
@@ -83,24 +70,28 @@ module APB_Master (
         state_next      = state;
         temp_addr_next  = temp_addr_reg;
         temp_wdata_next = temp_wdata_reg;
-        temp_we_next    = temp_we_reg;
-        decoer_en       = 1'b0;
+        temp_write_next = temp_write_reg;
+        PADDR           = temp_addr_reg;
+        PWDATA          = temp_wdata_reg;
+        PWRITE          = 1'b0;
         PENABLE         = 1'b0;
+        decoder_en      = 1'b0;
+
         case (state)
             IDLE: begin
-                decoer_en = 1'b0;
+                decoder_en = 1'b0;
                 if (transfer) begin
                     state_next      = SETUP;
-                    temp_addr_next  = addr;  // latching
+                    temp_addr_next  = addr;  // latching // 임시 저장소에 저장
                     temp_wdata_next = wdata;
-                    temp_we_next    = we;
+                    temp_write_next = write;
                 end
             end
             SETUP: begin
-                decoer_en = 1'b1;
-                PADDR = temp_addr_reg;
-                PENABLE = 1'b0;
-                if (temp_we_reg) begin
+                decoder_en = 1'b1;
+                PENABLE    = 1'b0;
+                PADDR      = temp_addr_reg;
+                if (temp_write_reg) begin
                     PWRITE = 1'b1;
                     PWDATA = temp_wdata_reg;
                 end else begin
@@ -109,51 +100,61 @@ module APB_Master (
                 state_next = ACCESS;
             end
             ACCESS: begin
-                PADDR = temp_addr_reg;
-                decoer_en = 1;
-                PENABLE = 1;
-                if (temp_we_reg) begin
+                decoder_en = 1'b1;
+                PENABLE    = 1'b1;
+                PADDR      = temp_addr_reg;
+                if (temp_write_reg) begin
                     PWRITE = 1'b1;
                     PWDATA = temp_wdata_reg;
                 end else begin
                     PWRITE = 1'b0;
                 end
-
-                if (PREADY1 & ~transfer) begin
+                if (ready) begin
                     state_next = IDLE;
-                    rdata = PRDATA1;
-                end else if (PREADY1 & transfer) begin
-                    state_next = SETUP;
-                    rdata = PRDATA1;
                 end
             end
         endcase
     end
 
+    APB_Decoder U_APB_Decoder (
+        .en (decoder_en),
+        .sel(temp_addr_reg),
+        .y  (pselx)
+    );
+
+    APB_Mux U_APB_Mux (
+        .sel  (temp_addr_reg),
+        .d0   (PRDATA0),
+        .d1   (PRDATA1),
+        .d2   (PRDATA2),
+        .d3   (PRDATA3),
+        .r0   (PREADY0),
+        .r1   (PREADY1),
+        .r2   (PREADY2),
+        .r3   (PREADY3),
+        .rdata(rdata),
+        .ready(ready)
+    );
+
 endmodule
 
-module APB_Master_Intf ();
-
-endmodule
 
 module APB_Decoder (
     input  logic        en,
     input  logic [31:0] sel,
     output logic [ 3:0] y
 );
-
     always_comb begin
-        y = 4'b0;
+        y = 4'b0;  //en 안들어오면 선택 하지 않는다.
         if (en) begin
-            case (sel)
-                32'h1000_0xxx: y = 4'b0001;
+            casex (sel)
+                32'h1000_0xxx: y = 4'b0001;  //32'h1000_0000 ~ 32'h1000_0fff
                 32'h1000_1xxx: y = 4'b0010;
                 32'h1000_2xxx: y = 4'b0100;
                 32'h1000_3xxx: y = 4'b1000;
             endcase
         end
     end
-
 endmodule
 
 module APB_Mux (
@@ -172,21 +173,21 @@ module APB_Mux (
 
     always_comb begin
         rdata = 32'bx;
-        case (sel)
+        casex (sel)
             32'h1000_0xxx: rdata = d0;
             32'h1000_1xxx: rdata = d1;
             32'h1000_2xxx: rdata = d2;
             32'h1000_3xxx: rdata = d3;
         endcase
     end
+
     always_comb begin
         ready = 1'bx;
-        case (sel)
+        casex (sel)
             32'h1000_0xxx: ready = r0;
             32'h1000_1xxx: ready = r1;
             32'h1000_2xxx: ready = r2;
             32'h1000_3xxx: ready = r3;
         endcase
     end
-
 endmodule
