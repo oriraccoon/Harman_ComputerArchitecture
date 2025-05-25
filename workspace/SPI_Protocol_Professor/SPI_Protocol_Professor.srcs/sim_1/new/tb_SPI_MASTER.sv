6dc6a8eb117ab8050821e31c8771c9f10edb8222
kccistc <djdjskskalal@gmail.com>
2025-05-25 20:02:08 +0900

    Auto commit for modified projects on 2025-05-25 20:02

--- workspace/SPI_Protocol_Professor/SPI_Protocol_Professor.srcs/sim_1/new/tb_SPI_MASTER.sv
+++ workspace/SPI_Protocol_Professor/SPI_Protocol_Professor.srcs/sim_1/new/tb_SPI_MASTER.sv
@@ -15,13 +15,13 @@ module tb_SPI_Master ();
     logic       SS;
     logic       so_done;
 
-
+    logic [7:0] data;
     spi_top dut (.*);
 
     always #5 clk = ~clk;
 
     initial begin
-        clk   = 0;
+        clk   = 0;data = 8'b0001_0000;
         reset = 1;
         #10;
         reset = 0;
@@ -39,20 +39,20 @@ module tb_SPI_Master ();
             SS = 0;
             @(posedge clk);
             start = 0;
-            repeat (799) @(posedge clk);
+            wait(done);
             @(posedge clk);
 
             // write data byte on 0x01 address
 
             @(posedge clk);
-            tx_data = 8'b1111_1111;
+            tx_data = data;
             start = 1;
             cpol = 0;
             cpha = 0;
             SS = 0;
             @(posedge clk);
             start = 0;
-            repeat (799) @(posedge clk);
+            wait(done);
             @(posedge clk);
 
             // address byte
@@ -65,22 +65,21 @@ module tb_SPI_Master ();
             SS = 0;
             @(posedge clk);
             start = 0;
-            repeat (799) @(posedge clk);
+            wait(done);
             @(posedge clk);
 
             @(posedge clk);
             start = 1;
             @(posedge clk);
             start = 0;
-            repeat (701) @(posedge clk);
-            @(posedge clk);
-            repeat (97) @(posedge clk);
+            wait(done);
             @(posedge clk);
 
             // spi_item.print(uvm_default_line_printer);
             
             @(posedge clk);
             tx_data = 8'b0001_0000;
+            data = data + tx_data;
         end
 
         #200 $finish;
